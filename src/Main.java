29fdf3a808d112964d62fc7064fcf0e1b1a6e9b3
iMohsen <iMohsen2002@gmail.com>
2023-03-25 13:17:35 +0330

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Airline Reservation Project - Yazd University - AP CE 2023");
+        System.out.println("Project start ...");
+        System.out.println("^_^");
+
+    }
+}
